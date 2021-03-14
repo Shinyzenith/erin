9b5f5e6d3127b64c2f23e79772315c67df4a81b5
AakashSharma7269 <aakashsensharma04@gmail.com>
2021-03-14 12:27:15 +0530

    e

--- src/cogs/nsfw.py
+++ src/cogs/nsfw.py
@@ -16,120 +16,135 @@ class NSFW(commands.Cog):
 
     @commands.cooldown(1, 5, commands.BucketType.user)
     @commands.command()
-    @commands.is_nsfw()
     async def hentai(self, ctx):
-        response = requests.get("https://shiro.gg/api/images/nsfw/hentai")
-        realResponse = response.json()
-        embed = discord.Embed(
-            title = "Juicy henti for you!",
-            color = 0xFFC0CB
-        )
-        embed.set_image(url = realResponse['url'])
-        await ctx.message.reply(embed = embed)
+        if ctx.channel.is_nsfw():
+            response = requests.get("https://shiro.gg/api/images/nsfw/hentai")
+            realResponse = response.json()
+            embed = discord.Embed(
+                title = "Juicy henti for you!",
+                color = 0xFFC0CB
+            )
+            embed.set_image(url = realResponse['url'])
+            await ctx.message.reply(embed = embed)
+        else:
+            await ctx.message.reply("This command can only be used in a NSFW channel.")
             
     @commands.cooldown(1, 5, commands.BucketType.user)
     @commands.command()
-    @commands.is_nsfw()
     async def cum(self, ctx):
-        response = requests.get("https://nekos.life/api/v2/img/cum")
-        realResponse = response.json()
-        embed = discord.Embed(
-            title = "sticky white stuff!",
-            color = 0xFFC0CB
-        )
-        embed.set_image(url = realResponse['url'])
-        await ctx.message.reply(embed = embed)
+        if ctx.channel.is_nsfw():
+            response = requests.get("https://nekos.life/api/v2/img/cum")
+            realResponse = response.json()
+            embed = discord.Embed(
+                title = "sticky white stuff!",
+                color = 0xFFC0CB
+            )
+            embed.set_image(url = realResponse['url'])
+            await ctx.message.reply(embed = embed)
+        else:
+            await ctx.message.reply("This command can only be used in a NSFW channel.")
 
     @commands.cooldown(1, 5, commands.BucketType.user)
     @commands.command()
-    @commands.is_nsfw()
     async def thighs(self, ctx):
-        response = requests.get("https://shiro.gg/api/images/nsfw/thighs")
-        realResponse = response.json()
-        embed = discord.Embed(
-            title = "Thic thighs!",
-            color = 0xFFC0CB
-        )
-        embed.set_image(url = realResponse['url'])
-        await ctx.message.reply(embed = embed)
+        if ctx.channel.is_nsfw():
+            response = requests.get("https://shiro.gg/api/images/nsfw/thighs")
+            realResponse = response.json()
+            embed = discord.Embed(
+                title = "Thic thighs!",
+                color = 0xFFC0CB
+            )
+            embed.set_image(url = realResponse['url'])
+            await ctx.message.reply(embed = embed)
+        else:
+            await ctx.message.reply("This command can only be used in a NSFW channel.")
 
     @commands.cooldown(1, 5, commands.BucketType.user)
     @commands.command(name="nekogif",aliases=['neko'])
-    @commands.is_nsfw()
     async def nekogif(self, ctx):
-        response = requests.get("https://nekos.life/api/v2/img/nsfw_neko_gif")
-        realResponse = response.json()
-        embed = discord.Embed(
-            title = "Gifs!",
-            color = 0xFFC0CB
-        )
-        embed.set_image(url = realResponse['url'])
-        await ctx.message.reply(embed = embed)
+        if ctx.channel.is_nsfw():
+            response = requests.get("https://nekos.life/api/v2/img/nsfw_neko_gif")
+            realResponse = response.json()
+            embed = discord.Embed(
+                title = "Gifs!",
+                color = 0xFFC0CB
+            )
+            embed.set_image(url = realResponse['url'])
+            await ctx.message.reply(embed = embed)
+        else:
+            await ctx.message.reply("This command can only be used in a NSFW channel.")
 
     @commands.cooldown(1, 5, commands.BucketType.user)
     @commands.command()
-    @commands.is_nsfw()
     async def boobs(self, ctx):
-        response = requests.get("https://nekos.life/api/v2/img/boobs")
+        if ctx.channel.is_nsfw():
+            response = requests.get("https://nekos.life/api/v2/img/boobs")
 
-        realResponse = response.json()
+            realResponse = response.json()
 
-        embed = discord.Embed(
-            title = "boooooobs!",
-            color = 0xFFC0CB
-        )
-        embed.set_image(url = realResponse['url'])
+            embed = discord.Embed(
+                title = "boooooobs!",
+                color = 0xFFC0CB
+            )
+            embed.set_image(url = realResponse['url'])
 
-        await ctx.message.reply(embed = embed)
+            await ctx.message.reply(embed = embed)
+        else:
+            await ctx.message.reply("This command can only be used in a NSFW channel.")
 
     @commands.cooldown(1, 5, commands.BucketType.user)
     @commands.command()
-    @commands.is_nsfw()
     async def blowjob(self, ctx):
-        response = requests.get("https://nekos.life/api/v2/img/blowjob")
+        if ctx.channel.is_nsfw():
+            response = requests.get("https://nekos.life/api/v2/img/blowjob")
 
-        realResponse = response.json()
+            realResponse = response.json()
 
-        embed = discord.Embed(
-            title = "Yummy!",
-            color = 0xFFC0CB
-        )
-        embed.set_image(url = realResponse['url'])
+            embed = discord.Embed(
+                title = "Yummy!",
+                color = 0xFFC0CB
+            )
+            embed.set_image(url = realResponse['url'])
 
-        await ctx.message.reply(embed = embed)
+            await ctx.message.reply(embed = embed)
+        else:
+            await ctx.message.reply("This command can only be used in a NSFW channel.")
 
     @commands.cooldown(1, 5, commands.BucketType.user)
     @commands.command()
-    @commands.is_nsfw()
     async def pussy(self, ctx):
-        response = requests.get("https://nekos.life/api/v2/img/pussy")
+        if ctx.channel.is_nsfw():
+            response = requests.get("https://nekos.life/api/v2/img/pussy")
 
-        realResponse = response.json()
+            realResponse = response.json()
 
-        embed = discord.Embed(
-            title = "Yummy!",
-            color = 0xFFC0CB
-        )
-        embed.set_image(url = realResponse['url'])
+            embed = discord.Embed(
+                title = "Yummy!",
+                color = 0xFFC0CB
+            )
+            embed.set_image(url = realResponse['url'])
 
-        await ctx.message.reply(embed = embed)
+            await ctx.message.reply(embed = embed)
+        else:
+            await ctx.message.reply("This command can only be used in a NSFW channel.")
 
     @commands.cooldown(1, 5, commands.BucketType.user)
     @commands.command()
-    @commands.is_nsfw()
     async def spank(self, ctx, user: commands.Greedy[discord.Member] = None):
-        if user == None:
-            await ctx.message.reply("Who do you want to spank?.")
-            return
-        response = requests.get("https://nekos.life/api/v2/img/spank").json()
-        spanked_users="".join([f"{users.mention} " for users in user])
-        embed = discord.Embed(
-            title = "Damn!",
-            description = f"{spanked_users} got spanked by {ctx.author.mention}",
-            color = 0xFFC0CB
-        )
-        embed.set_image(url = response['url'])
-        await ctx.message.reply(embed = embed)
-
+        if ctx.channel.is_nsfw():
+            if user == None:
+                await ctx.message.reply("Who do you want to spank?.")
+                return
+            response = requests.get("https://nekos.life/api/v2/img/spank").json()
+            spanked_users="".join([f"{users.mention} " for users in user])
+            embed = discord.Embed(
+                title = "Damn!",
+                description = f"{spanked_users} got spanked by {ctx.author.mention}",
+                color = 0xFFC0CB
+            )
+            embed.set_image(url = response['url'])
+            await ctx.message.reply(embed = embed)
+        else:
+            await ctx.message.reply("This command can only be used in a NSFW channel.")
 def setup(client):
     client.add_cog(NSFW(client))
