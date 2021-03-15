270a86946d574e46f36e7b561d390a29ab7ba01e
AakashSharma7269 <aakashsensharma04@gmail.com>
2021-03-15 16:23:03 +0530

    Optimized OwO command lmfaooooo

--- src/cogs/fun.py
+++ src/cogs/fun.py
@@ -3,42 +3,24 @@ import datetime
 import random
 import aiohttp
 import requests
+import json
+import asyncio
 import os
 from discord.ext import commands
 
 class Fun(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
-		
+	@commands.cooldown(5,7,commands.BucketType.user)
 	@commands.command(name="furrify",aliases=['owo','uwu'])
 	async def furrify(self, ctx, *, msg):
-
-		vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-
-		def last_replace(s, old, new):
-			li = s.rsplit(old, 1)
-			return new.join(li)
-
-		def text_to_owo(text):
-			smileys = [';;w;;', '^w^', '>w<', 'UwU', '(・`ω\´・)', '(´・ω・\`)']
-
-			text = text.replace('L', 'W').replace('l', 'w')
-			text = text.replace('R', 'W').replace('r', 'w')
-
-			text = last_replace(text, '!', '! {}'.format(random.choice(smileys)))
-			text = last_replace(text, '?', '? owo')
-			text = last_replace(text, '.', '. {}'.format(random.choice(smileys)))
-
-			for v in vowels:
-				if 'n{}'.format(v) in text:
-					text = text.replace('n{}'.format(v), 'ny{}'.format(v))
-				if 'N{}'.format(v) in text:
-					text = text.replace('N{}'.format(v), 'N{}{}'.format('Y' if v.isupper() else 'y', v))
-
-			return text
-
-		await ctx.send(text_to_owo(msg))        
-				
+		async with aiohttp.ClientSession() as session:
+			async with session.get(f"https://nekos.life/api/v2/owoify?text={msg}") as response:
+				response=await response.read()
+				response=response.decode("utf-8")
+				response=json.loads(response)
+				return await ctx.message.reply(response['owo'])
+						
 	@commands.cooldown(1, 5, commands.BucketType.user)
 	@commands.command()
 	async def neko(self, ctx):
