a506afac06f021327880bf86a17423a58ef588e5
AakashSharma7269 <aakashsensharma04@gmail.com>
2021-03-18 22:24:31 +0530

    help command updated

--- src/cogs/help.py
+++ src/cogs/help.py
@@ -26,9 +26,12 @@ class Help(commands.Cog):
 	async def on_ready(self):
 		log.warn(f"{self.__class__.__name__} Cog has been loaded")
 	@commands.command()
-	async def help(self, ctx, *input):
+	async def help(self, ctx, *,input:str=None):
 		if not input:
-			emb = discord.Embed(title='Commands and modules', color=ctx.message.author.color,description=f'Use `{ctx.prefix}help <module>` to gain more information about that module :smiley:\n')
+			emb = discord.Embed(title='Commands and modules', color=ctx.message.author.color,description=f'Use `{ctx.prefix}help <module>` to gain more information about that module <:Kanna:822144873170731018>\n')
+			emb.set_footer(text=f"Requested by {ctx.message.author}",icon_url=self.bot.user.avatar_url)
+			emb.set_thumbnail(url=ctx.message.author.avatar_url)
+			emb.set_author(name=ctx.message.author.display_name,icon_url=ctx.message.guild.icon_url)
 
 			cogs_desc = ''
 			for cog in self.bot.cogs:
@@ -42,12 +45,19 @@ class Help(commands.Cog):
 					commands_desc += f'{command.name} - {command.help}\n'
 			if commands_desc:
 				emb.add_field(name='Not belonging to a module', value=commands_desc, inline=False)
+			return await send_embed(ctx, emb)
 
-		elif len(input) == 1:
+		else:
 			pages=[]
+			def make_embed():
+				embed=Embed(color=ctx.message.author.color,timestamp=ctx.message.created_at)
+				embed.set_footer(text=f"Requested by {ctx.message.author}",icon_url=self.bot.user.avatar_url)
+				embed.set_thumbnail(url=ctx.message.author.avatar_url)
+				embed.set_author(name=ctx.message.author.display_name,icon_url=ctx.message.guild.icon_url)
+				return embed
+
 			for cog in self.bot.cogs:
-				if cog.lower() == input[0].lower():
-					# getting commands from cog
+				if cog.lower() == input.lower():
 					threshold=10
 					commands=self.bot.get_cog(cog).get_commands()
 					command_chunk=[commands[i:i + threshold] for i in range(0, len(commands), threshold)]
@@ -55,39 +65,45 @@ class Help(commands.Cog):
 					embeds=[]
 					command_list=""
 					for chunk in command_chunk:
+						embed = make_embed()
 						for command in chunk:
-								if command.hidden:
-									continue
-								i = i + 1
-								command_list = command_list + f'{i}) ``{ctx.prefix}{command.name}``\n'
-						embed=Embed(title=f"{command.cog_name} - Commands", description=self.bot.cogs[cog].__doc__,color=ctx.message.author.color,timestamp=ctx.message.created_at)
-						embed.set_footer(text=f"Requested by {ctx.message.author}",icon_url=self.bot.user.avatar_url)
-						embed.set_thumbnail(url=ctx.message.author.avatar_url)
-						embed.set_author(name=ctx.message.author.display_name,icon_url=ctx.message.guild.icon_url)
-						embed.add_field(inline=False,name="Command list",value=command_list)
+							embed.title=f"{command.cog_name} - Commands"
+							embed.description=self.bot.cogs[cog].__doc__
+							embed.set_footer(text='<> Denotes required argument. [] Denotes optional argument')
+							if command.hidden:
+								continue
+							i = i + 1
+							aliases=""
+							description=""
+							if len(command.aliases)!=0:
+								aliases=", ".join([alias for alias in command.aliases])
+							else: aliases="None"
+							description=command.description or "None"
+
+							command_list = command_list + f'{i}) ``{ctx.prefix}{command.name}``\n'
+							embed.add_field(inline=False,name=f"`{ctx.prefix}{command.name}`",value=f"Aliases: **{aliases}**\nDescription: **{description}**\nUsage: **{ctx.prefix+command.name} {command.signature}**")
+							print(dir(command))
+							print("\n\n")
 						embeds.append(embed)
 						embed=None
 						command_list=""
-					paginator = DiscordUtils.Pagination.CustomEmbedPaginator(ctx, remove_reactions=True)
-					paginator.add_reaction('⏮️', "first")
-					paginator.add_reaction('⏪', "back")
-					paginator.add_reaction('⏩', "next")
-					paginator.add_reaction('⏭️', "last")
-					paginator.add_reaction('🔐', "lock")
-					await paginator.run(embeds)
+					if len(embeds)==1:
+						return await ctx.send(embed=embeds[0])
+					else:
+						paginator = DiscordUtils.Pagination.CustomEmbedPaginator(ctx, remove_reactions=True)
+						paginator.add_reaction('⏮️', "first")
+						paginator.add_reaction('⏪', "back")
+						paginator.add_reaction('⏩', "next")
+						paginator.add_reaction('⏭️', "last")
+						paginator.add_reaction('❌', "lock")
+						return await paginator.run(embeds)
 					break
 
 			else:
-				emb = discord.Embed(title="What's that?!",
-									description=f"I've never heard from a module called `{input[0]}` before :scream:",
-									color=discord.Color.orange())
+				emb = discord.Embed(title="lol, sadphroge 😔😔😔",
+									description=f"ERROR 404 couldn't find `{input}` module",
+									color=ctx.message.author.color)
 				return await send_embed(ctx, emb)
-		elif len(input) > 1:
-			emb = discord.Embed(title="That's too much.",
-								description="Please request only one module at once :sweat_smile:",
-								color=discord.Color.orange())
-			return await send_embed(ctx, emb)
-
 
 def setup(bot):
 	bot.add_cog(Help(bot))
