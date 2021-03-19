a3df34968c1a86f81cbbeddce439659180904ccb
AakashSharma7269 <aakashsensharma04@gmail.com>
2021-03-19 21:21:16 +0530

    changed dist to src

--- starterin.sh
+++ starterin.sh
@@ -1,3 +1,3 @@
-forever start -a -l /home/aakash/erin/logs/erin.log -e err.log -c python3 --sourceDir /home/aakash/erin/dist/ main.py
+forever start -a -l /home/aakash/erin/logs/erin.log -e err.log -c python3 --sourceDir /home/aakash/erin/src/ main.py
 sleep 1
 echo "DONE"
