94ddc74bdc0cc4edbe5b4071fd05ab12980e008c
AakashSharma7269 <aakashsensharma04@gmail.com>
2021-04-04 00:38:33 +0530

    Final express server directory commit

---
+++ dashboard/dist/startserver.js
@@ -0,0 +1,31 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const express_handlebars_1 = __importDefault(require("express-handlebars"));
+dotenv_1.default.config({ "path": path_1.default.join(__dirname, "../../.env") });
+const app = express_1.default();
+const port = process.env.SERVER_PORT || 8080;
+//Handlebars middleware
+app.engine('handlebars', express_handlebars_1.default({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.set("views", path_1.default.join(__dirname, "./views"));
+app.get('/', (req, res) => {
+    res.render('index', {
+        layout: 'home',
+        content: 'OwO wot dis >~<'
+    });
+});
+app.get('/alt', (req, res) => {
+    res.render('index', {
+        layout: 'test',
+        content: 'OwO wot dis >~<'
+    });
+});
+app.use(express_1.default.static(path_1.default.join(__dirname + "./../dist")));
+//@TODO: SETUP ./routes instead of this one dirty huge ass file for all the routes > ~ <
+app.listen(port, () => console.log(`Server started at http://localhost:${port}`));
