cd15b3038a3bada56dbb359f83d6438b115c7596
AakashSharma7269 <aakashsensharma04@gmail.com>
2021-04-03 16:56:24 +0530

    Webserver update

--- dashboard/js/startserver.js
+++ dashboard/js/startserver.js
@@ -5,15 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var path_1 = __importDefault(require("path"));
+var dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config({ "path": path_1.default.join(__dirname, "../../.env") });
 var app = express_1.default();
-var port = process.env.PORT || 8080;
-// Configure Express to use EJS
-app.set("views", path_1.default.join(__dirname, "dashboard/html/"));
+var port = process.env.SERVER_PORT || 8080;
+app.set("views", path_1.default.join(__dirname, "./../html/"));
 app.set("view engine", "ejs");
-// define a route handler for the default home page
 app.get("/", function (req, res) {
     res.render("index");
 });
 app.listen(port, function () {
-    console.log("server started at http://localhost:" + port);
+    console.log("Server started at http://localhost:" + port);
 });
--- typescript/ts/startserver.ts
+++ typescript/ts/startserver.ts
@@ -1,18 +1,17 @@
 import express from "express";
 import path from "path";
+import dotenv from "dotenv";
 
+dotenv.config({"path":path.join(__dirname,"../../.env")});
 const app = express();
-const port = process.env.PORT || 8080; 
+const port = process.env.SERVER_PORT || 8080; 
 
-// Configure Express to use EJS
-app.set( "views", path.join( __dirname, "dashboard/html/" ) );
+app.set( "views", path.join( __dirname, "./../html/" ) );
 app.set( "view engine", "ejs" );
-
-// define a route handler for the default home page
 app.get( "/", ( req, res ) => {
     res.render( "index" );
 } );
 
 app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
+    console.log( `Server started at http://localhost:${ port }` );
 } );
