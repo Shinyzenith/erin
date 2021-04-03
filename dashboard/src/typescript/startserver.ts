3326d1eaa08d12a71efa7110a7b42e7ead41237b
AakashSharma7269 <aakashsensharma04@gmail.com>
2021-04-03 19:34:41 +0530

    Some minor changes

--- dashboard/src/typescript/startserver.ts
+++ dashboard/src/typescript/startserver.ts
@@ -3,15 +3,17 @@ import path from "path";
 import dotenv from "dotenv";
 
 dotenv.config({"path":path.join(__dirname,"../../../.env")});
-const app = express();
+
+const app:express.Application = express();
 const port = process.env.SERVER_PORT || 8080; 
 
-app.set( "views", path.join( __dirname, "./../html/" ) );
-app.set( "view engine", "ejs" );
-app.get( "/", ( req, res ) => {
-    res.render( "index" );
-} );
+// app.set( "views", path.join( __dirname, "./../html/" ) );
+// app.set( "view engine", "ejs" );
 
-app.listen( port, () => {
-    console.log( `Server started at http://localhost:${ port }` );
-} );
+// app.get( "/", ( req:express.Request, res:express.Response) => {
+//     res.render( "index" );
+// });
+
+app.use(express.static(path.join(__dirname,'./../../dist/')))
+
+app.listen( port, () => console.log( `Server started at http://localhost:${ port }`));
