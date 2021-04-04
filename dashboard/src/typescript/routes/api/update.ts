f2014975af4ca63a1968071eabe6c2b044c0e8e3
AakashSharma7269 <aakashsensharma04@gmail.com>
2021-04-04 17:57:42 +0530

    removed the incomplete async function

--- dashboard/src/typescript/routes/api/update.ts
+++ dashboard/src/typescript/routes/api/update.ts
@@ -1,10 +1,6 @@
 import Express from 'express';
 const router:Express.Router = Express.Router();
 
-async function fetchData(gid:number){
-
-}
-
 router.get('/',(req:Express.Request,res:Express.Response)=>{
     res.send({'msg':'update api endpoint is still in prod'})
 });
