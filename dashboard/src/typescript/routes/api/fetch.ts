16522da87b612304557a084c037a5e534ecd7533
AakashSharma7269 <aakashsensharma04@gmail.com>
2021-04-04 16:03:30 +0530

    added boilerplate code to the empty ts files and install mongoose with yarn

---
+++ dashboard/src/typescript/routes/api/fetch.ts
@@ -0,0 +1,8 @@
+import Express from 'express';
+const router = Express.Router();
+
+router.get('/',(req:Express.Request,res:Express.Response)=>{
+    //will add logic soon
+});
+
+module.exports = router;
---
+++ dashboard/src/typescript/routes/api/update.ts
@@ -0,0 +1,12 @@
+import Express from 'express';
+const router = Express.Router();
+
+async function fetchData(gid:number){
+    
+}
+
+router.get('/',(req:Express.Request,res:Express.Response)=>{
+    //will add logic soon
+});
+
+module.exports = router;
