e5a82bf0dea94548a4658b61571f35919404c9bc
AakashSharma7269 <aakashsensharma04@gmail.com>
2021-04-07 14:14:22 +0530

    meh ngl this is still kinda gay....i'll make it better when i feel like it :))

--- dashboard/src/typescript/routes/404page.ts
+++ dashboard/src/typescript/routes/404page.ts
@@ -1,8 +1,6 @@
 //404 error page not found middleware
 import * as express from 'express';
-// const router:express.Router = express.Router();
 
-// router.use
 const error404page=(req:express.Request,res:express.Response)=>{
     let invokedUrl=req.originalUrl;
     if(invokedUrl.endsWith('/')){
