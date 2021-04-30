a2537c4b3a1830c1d2cbe18369806306c94ee7f9
AakashSharma7269 <aakashsensharma04@gmail.com>
2021-05-01 01:08:27 +0530

    prettier lol

--- dashboard/src/typescript/routes/404page.ts
+++ dashboard/src/typescript/routes/404page.ts
@@ -1,13 +1,16 @@
 //404 error page not found middleware
-import * as express from 'express';
+import * as express from "express";
 
-const error404page=(req:express.Request,res:express.Response)=>{
-    let invokedUrl=req.originalUrl;
-    if(invokedUrl.endsWith('/')){
-        invokedUrl=invokedUrl.slice(0,-1)
-    }
-    res.status(400).render('NotFound',{'layout':'NotFound','name':`${invokedUrl}`})
-    return;
-}
+const error404page = (req: express.Request, res: express.Response) => {
+  let invokedUrl = req.originalUrl;
+  if (invokedUrl.endsWith("/")) {
+    invokedUrl = invokedUrl.slice(0, -1);
+  }
+  res
+    .status(400)
+    .render("NotFound", { layout: "NotFound", name: `${invokedUrl}` });
+  return;
+};
 
 module.exports = error404page;
+
