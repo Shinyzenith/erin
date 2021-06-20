a8d9f6454740ff2e1298719620bd2c423eb64f32
actualdankcoder <zubi2048@gmail.com>
2021-06-20 12:30:53 +0530

    patch prune

--- src/cogs/mod.py
+++ src/cogs/mod.py
@@ -1341,7 +1341,10 @@ class Moderation(commands.Cog):
                 return True
             else:
                 return False
-        deleted = await ctx.channel.purge(limit=amount, check=check)
+        try:
+            deleted = await ctx.channel.purge(limit=amount, check=check)
+        except:
+            pass
 # TODO if bot if offline and they're muted with lets say something like another bot and then they're meant to be unmuted with erin then we don't have the log in mutes collection so we need to make sure that if they have the role then we'll just try to remove it and add a warn owo
 # TODO ability to add a mod log channel and write an async handler to webhook the data to the channel.
 # TODO: 2) TEMPBAN 5) invite lookup
