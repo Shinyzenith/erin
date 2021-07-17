63fa3563d2066e077338c5f7b5130ece6bd7acfb
thurm64 <63270342+thurm64@users.noreply.github.com>
2021-07-17 13:58:52 -0400

    docstring moment

--- src/cogs/image.py
+++ src/cogs/image.py
@@ -586,6 +586,9 @@ async def daviesad(ctx, args):
 
 
 class Image(commands.Cog):
+    """
+     Meme maker <:lmao:865969693755310101>
+     """
     def __init__(self, bot):
         self.bot = bot
 
