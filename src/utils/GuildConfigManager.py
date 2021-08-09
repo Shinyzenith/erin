d6a385eaef0357371e71c37525953111d33f2f19
AakashSharma7269 <aakashsensharma@gmail.com>
2021-08-09 15:57:12 +0530

    fixed function parameters (cleaned them up and added type definitions) and removed a useless parameter i had kept in the set_default_mutetime function.

--- src/utils/GuildConfigManager.py
+++ src/utils/GuildConfigManager.py
@@ -2,8 +2,6 @@ import os
 import discord
 import motor.motor_asyncio
 
-# This is a work in progress cog, we'll add more functions to this class!
-
 class GuildConfigManager:
     """
     Guild config util class for erin
@@ -15,105 +13,105 @@ class GuildConfigManager:
         self.db = self.client.erin
         self.col = self.db["config"]
 
-    async def register_guild(self, g, recheck=True):
+    async def register_guild(self, guild_object:discord.Guild, recheck=True):
         if recheck:
-            guild = await self.col.find_one({"gid": g.id})
+            guild = await self.col.find_one({"gid": guild_object.id})
             if not guild:
-                guild = {"gid": g.id, "prefixes": ["-"]}
+                guild = {"gid": guild_object.id, "prefixes": ["-"]}
                 await self.col.insert_one(guild)
         else:
-            guild = {"gid": g.id, "prefixes": ["-"]}
+            guild = {"gid": guild_object.id, "prefixes": ["-"]}
             await self.col.insert_one(guild)
         return guild
 
-    async def unregister_guild(self, g, recheck=True):
+    async def unregister_guild(self, guild_object:discord.Guild, recheck=True):
         if recheck:
-            guild = await self.col.find_one({"gid": g.id})
+            guild = await self.col.find_one({"gid": guild_object.id})
             if guild:
-                await self.col.delete_one({"gid": g.id})
+                await self.col.delete_one({"gid": guild_object.id})
         else:
-            await self.col.delete_one({"gid": g.id})
+            await self.col.delete_one({"gid": guild_object.id})
 
-    async def update_guild(self, g, document):
-        guild = await self.register_guild(g)
-        await self.col.replace_one({"gid": g.id}, document)
+    async def update_guild(self, guild_object:discord.Guild, document):
+        guild = await self.register_guild(guild_object)
+        await self.col.replace_one({"gid": guild_object.id}, document)
         return guild
 
-    async def get_prefix(self, g):
+    async def get_prefix(self, guild_object:discord.Guild):
         prefixes = []
-        guild = await self.register_guild(g)
+        guild = await self.register_guild(guild_object)
         prefixes = guild["prefixes"]
         return prefixes
 
-    async def add_prefix(self, g, prefix):
-        guild = await self.register_guild(g)
+    async def add_prefix(self, guild_object:discord.Guild, prefix):
+        guild = await self.register_guild(guild_object)
         if prefix in guild["prefixes"]:
             return False
         else:
             guild["prefixes"].append(prefix)
-            await self.update_guild(g, guild)
+            await self.update_guild(guild_object, guild)
             return True
 
-    async def remove_prefix(self, g, prefix):
-        guild = await self.register_guild(g)
+    async def remove_prefix(self, guild_object:discord.Guild, prefix):
+        guild = await self.register_guild(guild_object)
         if not prefix in guild["prefixes"]:
             return False
         else:
             guild["prefixes"].remove(prefix)
-            await self.update_guild(g, guild)
+            await self.update_guild(guild_object, guild)
             return True
 
-    async def add_ban_appeal(self, g, ban_appeal: str):
-        guild = await self.register_guild(g)
+    async def add_ban_appeal(self, guild_object:discord.Guild, ban_appeal: str):
+        guild = await self.register_guild(guild_object)
         guild["ban_appeal"] = ban_appeal
-        await self.update_guild(g, guild)
+        await self.update_guild(guild_object, guild)
         return True
 
-    async def remove_ban_appeal(self, g):
-        guild = await self.register_guild(g)
+    async def remove_ban_appeal(self, guild_object:discord.Guild):
+        guild = await self.register_guild(guild_object)
         guild.pop("ban_appeal")
-        await self.update_guild(g, guild)
+        await self.update_guild(guild_object, guild)
         return True
 
-    async def get_ban_appeal(self, g):
-        guild = await self.register_guild(g)
+    async def get_ban_appeal(self, guild_object:discord.Guild):
+        guild = await self.register_guild(guild_object)
         link = guild["ban_appeal"]
         return link
 
-    async def add_muted_role(self, g, muted_role: int):
-        guild = await self.register_guild(g)
+    async def add_muted_role(self, guild_object:discord.Guild, muted_role: int):
+        guild = await self.register_guild(guild_object)
         guild["muted_role"] = muted_role
-        await self.update_guild(g, guild)
+        await self.update_guild(guild_object, guild)
         return True
 
-    async def remove_muted_role(self, g):
-        guild = await self.register_guild(g)
+    async def remove_muted_role(self, guild_object:discord.Guild):
+        guild = await self.register_guild(guild_object)
         guild.pop("muted_role")
-        await self.update_guild(g, guild)
+        await self.update_guild(guild_object, guild)
 
-    async def get_muted_role(self, g):
-        guild = await self.register_guild(g)
+    async def get_muted_role(self, guild_object:discord.Guild):
+        guild = await self.register_guild(guild_object)
         muted_role = guild["muted_role"]
         return muted_role
 
-    async def update_currency_channel(self, g, channelID: int):
-        guild = await self.register_guild(g)
+    async def update_currency_channel(self, guild_object:discord.Guild, channelID: int):
+        guild = await self.register_guild(guild_object)
         guild["channel"] = channelID
-        await self.update_guild(g, guild)
+        await self.update_guild(guild_object, guild)
         return True
 
-    async def remove_currency_channel(self, g):
-        guild = await self.register_guild(g)
+    async def remove_currency_channel(self, guild_object:discord.Guild):
+        guild = await self.register_guild(guild_object)
         guild.pop("channel")
-        await self.update_guild(g, guild)
+        await self.update_guild(guild_object, guild)
 
-    async def get_currency_channel(self, g):
-        guild = await self.register_guild(g)
+    async def get_currency_channel(self, guild_object:discord.Guild):
+        guild = await self.register_guild(guild_object)
         currencyChannnel = guild["channel"]
         return currencyChannnel
 
-    async def set_default_mutetime(self,g:discord.Guild,default:int=None,get = False):
-        guild = await self.register_guild(g)
+    async def set_default_mutetime(self,guild_object:discord.Guild,default:int=None):
+        guild = await self.register_guild(guild_object)
         current_mute_time=None # the current config
         try:
             current_mute_time=guild["default_mute_duration"]
@@ -121,17 +119,17 @@ class GuildConfigManager:
             pass
         if  current_mute_time==None and default==None:
             guild['default_mute_duration'] = 3600 # default mute time will be 1 hour if config var has not been set
-            return await self.update_guild(g,guild)
+            return await self.update_guild(guild_object,guild)
         elif default !=None:
             guild['default_mute_duration'] = default
-            return await self.update_guild(g,guild)
+            return await self.update_guild(guild_object,guild)
 
-    async def get_default_mutetime(self,g:discord.Guild):
-        guild = await self.register_guild(g)
+    async def get_default_mutetime(self,guild_object:discord.Guild):
+        guild = await self.register_guild(guild_object)
         try:
             current_mute_time=guild['default_mute_duration']
         except KeyError:
-            await self.set_default_mutetime(g)
-            current_mute_time = await self.register_guild(g)
+            await self.set_default_mutetime(guild_object)
+            current_mute_time = await self.register_guild(guild_object)
             current_mute_time=current_mute_time['default_mute_duration']
         return current_mute_time
